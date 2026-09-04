eac2f62a13b36a8c031b23b165f354ac257417ea
Borbobroste <nicolas.hossard@gmail.com>
2026-09-04 13:12:06 +0200

    Update text rendering to handle 'ils' replacement
    Replace occurrences of 'ils' with 'il​s' in text rendering functions.

--- util/tufteWrapper.js
+++ util/tufteWrapper.js
@@ -3,7 +3,9 @@ const parse = configureParser({ react: false })
 
 module.exports = {
   render: function(text, wrap = true) {
-    let tmp = parse(text)
+    let tmp = parse(String(text).replace(/\bils\b/g, 'il\u200Bs'))
+    tmp = tmp.replace(/\u200B/g, '')
+
     if (wrap && (tmp.indexOf("<section>") == -1)) {
        return ('<section>' + tmp + '</section>');
     }
@@ -13,6 +15,9 @@ module.exports = {
   },
   
   renderInline: function(text) {
-		return parse(String(text)).replace('<p>', '').replace('</p>', '');
+    let tmp = parse(String(text).replace(/\bils\b/g, 'il\u200Bs'))
+    tmp = tmp.replace(/\u200B/g, '')
+
+    return tmp.replace('<p>', '').replace('</p>', '');
   }
 }
